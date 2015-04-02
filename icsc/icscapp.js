31634d8499fe88cf4765d9cad4b7c5ca704f4f39
I20496 <saihong@icsc.com.tw>
2015-04-02 14:00:53 +0800

    'OKOK'

---
+++ icsc/icscapp.js
@@ -0,0 +1,20 @@
+var utils = (function() {
+    function loadModel(id, data) {
+        $('#' + id).gk('model', data[id]);
+    }
+
+    function refreshOpts(id, data) {
+    	var $sel = $('#'+id),
+    		sel = $sel[0] ;
+    	sel.options.length = 0;
+    	$.each(data, function(idx,obj){
+    		sel.add(new Option(obj.text, obj.value)) ;
+    	});
+    	$sel.selectmenu('refresh') ;
+    }
+
+    return {
+    	loadModel:loadModel,
+    	refreshOpts:refreshOpts
+    } ;
+})();
